01b3bf6e1f3c9f10cd895cbc991522bb116b0d14
alican-uzun <alican.uzun@bitaksi.com>
2022-10-17 01:24:42 +0300

    last changes

--- src/main/java/com/devops/hello/world/HomeController.java
+++ src/main/java/com/devops/hello/world/HomeController.java
@@ -12,12 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class HomeController{
+    /**
+     * @param testId
+     * @param testName
+     * @return
+     */
     @GetMapping
     public ResponseEntity<?> find(@RequestParam("testId") String testId, @RequestParam("testName") @Nullable String testName){
 
             System.out.println(testId);
             System.out.println(testName);
 
-            return ResponseEntity.ok("This is testId: " + testId + "\n" + " This is testName: " + testName);
+            return ResponseEntity.ok("This is testId: " + testId + "  |  " + " This is testName: " + testName);
     }
 }
